689a2999f90af11a7995a5b0fd089381a9e00175
Carlos Maldonado <karlozm@gmail.com>
2020-01-13 22:39:16 -0600

    filepath modified to work with child themes or parent themes automatically

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-# Develus Elementor Widgets
-
-A Wordpress Plugin with Widgets for Elementor.
-
-Install, Activate, Use it.
-
-###Widgets
-
-- PHP Includer
-
+# develus-elementor-widgets
--- widgets/php-includer/php-includer-render.php
+++ widgets/php-includer/php-includer-render.php
@@ -7,7 +7,13 @@ if ( empty( $settings['title'] ) ) {
 
 $title = $settings['title'];
 
-$filePath = get_template_directory().'/'.$title;
+if(is_child_theme()){
+	//$filePath = get_stylesheet_directory_uri().'/'.$title;
+	//                            return child-theme folder name
+	$filePath = get_theme_root().'/'.get_option('stylesheet').'/'.$title;
+}else{
+	$filePath = get_template_directory().'/'.$title;
+}
 
 if( file_exists($filePath) ){
 	include($filePath);
